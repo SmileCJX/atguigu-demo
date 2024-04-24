582d999ceb7e26319b42a89196d4fb96fc7b7e92
caijunxiang <1092925421@qq.com>
2024-04-24 23:50:41 +0800

    feat: 练习题

---
+++ chapter05/code5-6-practice.py
@@ -0,0 +1,21 @@
+# # m行n列*号矩阵
+# m = 5
+# n = 4
+# for i in range(m):
+#     print('*' * n)
+
+# 打印n行等腰三角形
+n = 8
+for i in range(n):
+    print(' ' * (n - 1 -i) +'*' * (i * 2 +1))
+
+# 穷举
+peach = 1
+while True:
+    d1 = peach // 2 - 1
+    d2 = d1 // 2 - 1
+    d3 = d2 // 2 - 1
+    if d3 == 1:
+        print(peach)
+        break
+    peach += 1
