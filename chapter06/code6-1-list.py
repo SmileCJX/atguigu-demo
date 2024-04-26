ca08864195673cd55b71a01496e9db3bde579c03
caijunxiang <1092925421@qq.com>
2024-04-27 00:19:25 +0800

    feat: 列表的常用方法

--- chapter06/code6-1-list.py
+++ chapter06/code6-1-list.py
@@ -24,7 +24,7 @@ print('0' not in list3)
 print('1' in [1, 2, 3, 4])
 print([1, 2, 3, 4] < [2, 1]) # 比较第一个字符
 
-# 内置函数
+# 内置函数 函数名()
 print(len(list3)) # 求元素个数
 
 print(max(list3)) # 求元素的最大值
@@ -43,3 +43,27 @@ for i, j in enumerate(list2):
 
 for i in range(len(list2)):
     print(i, list2[i])
+
+print('-' * 30)
+# 列表的常用方法 变量.方法名()
+list3.append('666')
+print(list3)
+# 添加列表
+list3.extend([1, 2, 3])
+print(list3)
+# 插入元素
+list3.insert(2, 'hello')
+print(list3)
+# 根据索引删除元素
+list3.pop(3)
+print(list3)
+# 根据元素删除
+list3.remove('7')
+print(list3)
+list3.append('hello')
+print(list3)
+list3.remove('hello')
+print(list3)
+# 清空列表
+list3.clear()
+print(list3)
