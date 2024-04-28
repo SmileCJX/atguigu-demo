6297c0715df51cd2b9d9a302c60c0eef4a61d332
caijunxiang <1092925421@qq.com>
2024-04-28 23:20:37 +0800

    feat: 类型的创建

--- chapter06/code6-2-tuple.py
+++ chapter06/code6-2-tuple.py
@@ -1,3 +1,4 @@
+# 类型的创建
 tuple1 = (1, 2, 3, True, 'hello')
 print(tuple1)
 print(type(tuple1))
@@ -7,3 +8,10 @@ print(list1)
 tuple2 = (1, ) # 元组里只有一个元素时， 加一个逗号
 print(tuple2)
 print(type(tuple2))
+
+tuple3 = tuple() # tuple()： 类型转换
+print(tuple3)
+print(type(tuple3))
+tuple4 = ()
+print(tuple4)
+print(type(tuple4))
