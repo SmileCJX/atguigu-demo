13bb66c57147a744362403f2718692ab6fbc237a
caijunxiang <1092925421@qq.com>
2024-04-30 22:31:47 +0800

    feat: 用户登录数据结构

---
+++ chapter06/code6-7-login-demo.py
@@ -0,0 +1,11 @@
+# 用户名、密码、黑名单
+# users = ['小红', 'mia', 'jack']
+# password  =['123', '456', '789']
+# not_allowed = ['jack']
+user = [
+    {'name': '小红', 'password': '123', 'status': True},
+    {'name': 'mia', 'password': '456',  'status': True},
+    {'name': 'jack', 'password': '789', 'status': False},
+]
+
+print(user)
