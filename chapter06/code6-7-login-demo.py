2b74c1052da85995a4a6ab274096b71f361f1f7d
caijunxiang <1092925421@qq.com>
2024-05-01 20:43:47 +0800

    feat: 用户登录系统

--- chapter06/code6-7-login-demo.py
+++ chapter06/code6-7-login-demo.py
@@ -2,11 +2,11 @@
 # users = ['小红', 'mia', 'jack']
 # password  =['123', '456', '789']
 # not_allowed = ['jack']
-users = [
-    {'name': '小红', 'password': '123', 'status': True},
-    {'name': 'mia', 'password': '456',  'status': True},
-    {'name': 'jack', 'password': '789', 'status': False},
-]
+users = {
+    '小红' : {'name': '小红', 'password': '123', 'status': True},
+    'mia' : {'name': 'mia', 'password': '456',  'status': True},
+    'jack' : {'name': 'jack', 'password': '789', 'status': False},
+}
 
 print(users)
 flag = False
@@ -14,20 +14,13 @@ for i in range(3):
     user = input('请输入你的用户名：')
     pwd = input('请输入你的密码：')
 
-    for i in users:
-        if user == i['name']:
-            if pwd == i['password']:
-                if i['status'] == True:
-                    print('登录成功!')
-                    flag = True
-                    break
-                else:
-                    print('账号失效，请联系管理员！')
-            else:
-                print('密码输入错误，请重试！')
-            break
+    if user in users and pwd == users[user]['password'] and users[user]['status']:
+        print('登录成功!')
+        break
+    elif user in users and not users[user]['status']:
+        print('账号失效，请联系管理员！')
+    elif user in users and pwd != users[user]['password']:
+        print('密码输入错误，请重试！')
     else:
         print('用户不存在，请先注册！')
-    if flag:
-        break
 
