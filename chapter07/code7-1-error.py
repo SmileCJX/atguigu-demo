118f01e485b71dc35985e4322acf527391eea659
caijunxiang <1092925421@qq.com>
2024-05-01 22:49:23 +0800

    feat: 常见错误类型

---
+++ chapter07/code7-1-error.py
@@ -0,0 +1,27 @@
+# NameError
+# prit('hello world') # 函数名拼写错误
+print('hello world')
+a = '111'
+# print(aa) # 变量名拼写错误
+print(a)
+# print(b) # 使用一个不存在的变量
+
+# SyntaxError IndentationError
+# if 'he' == 'hi':
+# print('hello')
+
+# TypeError
+# print(3 + '2')
+
+tp = (1, 2, 3)
+# tp[2] = 4
+# AttributeError
+# tp.append(2)
+print(tp)
+
+# KeyError
+# d = {1:2, 3:4}
+# print(d[2])
+
+# IndexError
+# print(tp[4])
