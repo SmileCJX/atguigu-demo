9af4ded66e5563978c0a29dbaf6d54403d1ea2a5
caijunxiang <1092925421@qq.com>
2024-05-02 22:34:30 +0800

    feat: 函数默认参数

--- chapter08/code8-1-func.py
+++ chapter08/code8-1-func.py
@@ -8,8 +8,30 @@ def sum_2(a, b): # 形式参数
     return a + b
 
 result = sum_2(6, 9) # 实际参数
-a = len('get')
-print(a)
-print(result)
+# a = len('get')
+# print(a)
+# print(result)
 
+def power(x, n = 2): # n: 默认参数，缺省参数
+    return x ** n
+
+a = power(4, 3)
+b = power(5, 3)
+c = power(6)
+print(a)
+print(b)
+print(c)
+a = int('16', 8)
+print(a)
+
+def infos(name, age = 24, gender = '女'):
+    return '大家好，我叫%s， 我今年%d岁，我是一名%s生' % (name, age, gender)
+
+
+s = infos('cjx', 30, '男')
+lily = infos('lily', 25)
+jack = infos('jack', 30, '男')
+jack = infos('jack', gender='男')
+print(s)
+print(jack)
 
