d4f1dd97107d65aa93a4e885e685786a666dbf58
caijunxiang <1092925421@qq.com>
2024-05-04 22:44:59 +0800

    feat: 匿名函数

--- chapter08/code8-3-lambda.py
+++ chapter08/code8-3-lambda.py
@@ -16,7 +16,28 @@ print(a)
 
 
 a = [1, 2, 3, 4, 5]
-def f(x):
-    return x ** 2
-result = map(f, a)
+result = map(lambda x:x ** 3, a) # 映射
 print(list(result))
+
+# reduce 规约
+from functools import reduce
+result = reduce(lambda x, y: x + y, a)
+print(result)
+
+result = 1
+a = [1, 2, 3, 4, 5]
+for i in a:
+    result *= i
+print(result)
+
+# filter 过滤
+# for i in a:
+#     if a % 2 == 0:
+#         b.append(i)
+
+a = [1, 2, 3, 4, 5]
+result = filter(lambda x: x % 2, a)
+print(list(result))
+
+a = [1, 2, 3, 40, 5, 6, 0, 6, 0, 5]
+result = filter()
