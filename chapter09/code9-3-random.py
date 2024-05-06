40c9057d861ed56a47e81af5b65f989501e47e04
caijunxiang <1092925421@qq.com>
2024-05-07 00:01:52 +0800

    feat: 随机数

--- chapter09/code9-3-random.py
+++ chapter09/code9-3-random.py
@@ -14,13 +14,21 @@ print(random.choice(list1))
 print(random.choice('hello'))
 
 print(ord('A'))
-n = 5
+
 # 生成一个随机字母组成的列表
-a = []
-for i in range(20):
-    s = ''
-    for j in range(n):
-        t = random.randint(65, 90)
-        s += chr(t)
-    a.append(s)
-print(a)
+# a = []
+# n = 5
+# for i in range(20):
+#     s = ''
+#     for j in range(n):
+#         t = random.randint(65, 90)
+#         s += chr(t)
+#     a.append(s)
+# print(a)
+
+print('*' * 30)
+from my_package import my_tools
+print(my_tools.random_string(5))
+
+random.shuffle(list1)
+print(list1)
