786bf3e0edb697e9d293f3a6317e27858a5f3197
caijunxiang <1092925421@qq.com>
2024-05-09 19:09:49 +0800

    feat: 读取文件

--- chapter10/code10-1-read-file.py
+++ chapter10/code10-1-read-file.py
@@ -8,7 +8,13 @@ filename = path + '/test.txt'
 f = open(filename, mode='r', encoding='utf-8') # 绝对路径
 
 # 读取文件
-context = f.read()
+# context = f.read(5) # 只读取部分数据，包括换行符
+# print(context)
+
+context = f.readline()
+print(context)
+
+context = f.readlines()
 print(context)
 
 # 关闭文件
