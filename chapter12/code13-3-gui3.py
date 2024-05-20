85e4dd4db2aaca099d83f9a7c14e16ea88be8111
caijunxiang <1092925421@qq.com>
2024-05-20 22:49:34 +0800

    feat: 抽奖器优化

--- chapter12/code13-3-gui3.py
+++ chapter12/code13-3-gui3.py
@@ -5,11 +5,23 @@ class MyFrame(wx.Frame):
     NameList = ['cjx', 'tom', 'jack']
     # 构造方案
     def __init__(self):
-        wx.Frame.__init__(self, None, title = '抽奖器')
+        wx.Frame.__init__(self, None, title = '抽奖器', pos = (100, 100), size = (400, 600))
         # 创建面板
-        self.pl = wx.Panel(self)
+        self.pl = wx.Panel(self, pos = (0, 0), size = (400, 600))
+        # 设置背景颜色
+        # self.SetBackgroundColour(wx.BLUE)
+        self.SetBackgroundColour((200, 255, 0))
+        # self.SetBackgroundColour('#00FF00')
         # 创建静态文本
-        self.staticText = wx.StaticText(self.pl, label = random.choice(self.NameList))
+        self.staticText = wx.StaticText(self.pl, label = random.choice(self.NameList), pos = (0, 50), size = (400, 400), style = wx.TE_CENTER)
+        # 创建自提
+        # 字体大小，字体包， 字体风格， 加粗
+        font = wx.Font(48
+                       , wx.FONTFAMILY_SWISS
+                       , wx.FONTSTYLE_NORMAL
+                       , wx.FONTWEIGHT_NORMAL)
+        # 静态文本设置成我们自己创建的字体
+        self.staticText.SetFont(font)
         # 创建按钮
         self.btn1 = wx.Button(self.pl, label = '开始抽奖', pos = (100, 200))
         self.btn2 = wx.Button(self.pl, label = '结束抽奖', pos = (200, 200))
