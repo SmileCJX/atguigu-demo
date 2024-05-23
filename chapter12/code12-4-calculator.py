0ce3bddde36dae835c2a07974e2e109e732f228b
caijunxiang <1092925421@qq.com>
2024-05-23 21:31:41 +0800

    feat: 计算器功能实现

--- chapter12/code12-4-calculator.py
+++ chapter12/code12-4-calculator.py
@@ -42,6 +42,84 @@ class MyFrame(wx.Frame):
         self.btn_0 = wx.Button(self.pl, label='0', pos = (self.pos_x, self.pos_y + 240), size = (self.btn_w, self.btn_h))
         self.btn_point = wx.Button(self.pl, label='.', pos = (self.pos_x + 180, self.pos_y + 240), size = (self.btn_w, self.btn_h))
 
+        # 绑定按钮对应的事件
+        self.Bind(wx.EVT_BUTTON, self.On_btn_clear, self.btn_clear)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_div, self.btn_div)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_mul, self.btn_mul)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_sub, self.btn_sub)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_add, self.btn_add)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_back, self.btn_back)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_eq, self.btn_eq)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_9, self.btn_9)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_8, self.btn_8)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_7, self.btn_7)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_6, self.btn_6)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_5, self.btn_5)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_4, self.btn_4)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_3, self.btn_3)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_2, self.btn_2)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_1, self.btn_1)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_0, self.btn_0)
+        self.Bind(wx.EVT_BUTTON, self.On_btn_point, self.btn_point)
+
+    def On_btn_0(self, event):
+        self.entry.AppendText("0")
+
+    def On_btn_1(self, event):
+        self.entry.AppendText("1")
+
+    def On_btn_2(self, event):
+        self.entry.AppendText("2")
+
+    def On_btn_3(self, event):
+        self.entry.AppendText("3")
+
+    def On_btn_4(self, event):
+        self.entry.AppendText("4")
+
+    def On_btn_5(self, event):
+        self.entry.AppendText("5")
+
+    def On_btn_6(self, event):
+        self.entry.AppendText("6")
+
+    def On_btn_7(self, event):
+        self.entry.AppendText("7")
+
+    def On_btn_8(self, event):
+        self.entry.AppendText("8")
+
+    def On_btn_9(self, event):
+        self.entry.AppendText("9")
+
+    def On_btn_sub(self, event):
+        self.entry.AppendText("-")
+
+    def On_btn_add(self, event):
+        self.entry.AppendText("+")
+
+    def On_btn_mul(self, event):
+        self.entry.AppendText("*")
+
+    def On_btn_div(self, event):
+        self.entry.AppendText("/")
+
+    def On_btn_eq(self, event):
+        text = self.entry.GetValue()
+        result = str(eval(text))
+        self.entry.SetValue(result)
+
+    def On_btn_back(self, event):
+        text = self.entry.GetValue()
+        result = str(eval(text))
+        self.entry.SetValue(result)
+
+    def On_btn_clear(self, event):
+        self.entry.Clear()
+
+    def On_btn_point(self, event):
+        self.entry.AppendText(".")
+
 if __name__ == "__main__":
     # 创建应用程序对象
     app = wx.App()
