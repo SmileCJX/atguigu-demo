7f33b54cef5030f1886bfc02a63abbb565bf811c
caijunxiang <1092925421@qq.com>
2024-05-25 13:48:01 +0800

    feat: 客户端聊天室

--- chapter13/code13-1-chat-client.py
+++ chapter13/code13-1-chat-client.py
@@ -11,16 +11,44 @@ class Client(wx.Frame):
         self.client_socket = None
 
         # 界面布局
-        wx.Frame.__init__(self, None, title = self.name + "聊天室客户端", size = (450, 600), pos = (100, 100))
+        wx.Frame.__init__(self, None, title = self.name + "聊天室客户端", size = (450, 660), pos = (100, 50))
         # 创建面板
         self.pl = wx.Panel(self)
         # 创建按钮
         # 加入聊天室
-        self.conn_btn = wx.Button(self.pl, label = '加入聊天室', pos = (10, 10), size = (200, 40))
+        self.conn_btn = wx.Button(self.pl, label='加入聊天室', pos=(10, 10), size=(200, 40))
         # 离开聊天室
-
+        self.dis_conn_btn = wx.Button(self.pl, label='离开聊天室', pos=(220, 10), size=(200, 40))
         # 清空按钮
+        self.clear_btn = wx.Button(self.pl, label='清空', pos=(10, 580), size=(200, 40))
         # 发送按钮
+        self.send_btn = wx.Button(self.pl, label='发送', pos=(220, 580), size=(200, 40))
+        # 创建聊天内容文本框
+        self.text = wx.TextCtrl(self.pl, size=(400, 400), pos=(10, 60), style=wx.TE_READONLY|wx.TE_MULTILINE)
+        # 创建输入文本框
+        self.input_text = wx.TextCtrl(self.pl, size=(400,100), pos=(10, 470), style=wx.TE_MULTILINE)
+
+        # 按钮的事件绑定
+        self.Bind(wx.EVT_BUTTON, self.clear, self.clear_btn)
+        self.Bind(wx.EVT_BUTTON, self.conn, self.conn_btn)
+        self.Bind(wx.EVT_BUTTON, self.dis_conn, self.dis_conn_btn)
+        self.Bind(wx.EVT_BUTTON, self.send, self.send_btn)
+
+    # 点击 加入聊天室 按钮 触发
+    def conn(self, event):
+        print('conn方法')
+
+    # 点击 离开聊天室 按钮 触发
+    def dis_conn(self, event):
+        print('dis_conn')
+
+    # 点击 清空 按钮 触发
+    def clear(self, event):
+        print('clear方法')
+
+    # 点击 发送 按钮 触发
+    def send(self, event):
+        print('send方法')
 
 # 程序入口
 if __name__ == '__main__':
